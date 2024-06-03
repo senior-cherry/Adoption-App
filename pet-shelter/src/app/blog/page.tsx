47f77bbe313ec3101c7d308c503a357800e10217
Олександр Пархоменко <42373411+senior-cherry@users.noreply.github.com>
2024-06-03 17:24:07 +0300

    check if posts available

--- pet-shelter/src/app/blog/page.tsx
+++ pet-shelter/src/app/blog/page.tsx
@@ -21,24 +21,29 @@ const Blog = async () => {
         return (
             <main className="mt-12">
                 <div className="blog">
-                    {posts.map((post) => (
-                                <div className="blog-post" key={post.id}>
-                                    <div className="blog-post-img">
-                                        <Image src={`/uploads/${post.imageUrl}`} alt={post.imageUrl}/>
-                                    </div>
-                                    <div className="blog-post-info">
-                                        <div className="blog-post-date">
-                                            <span>{post.createdAt.toString().substring(0, 10)}</span>
-                                        </div>
-                                        <Link href={`/blog/post/${post.id}`}>
-                                            <h1 className="blog-post-title">{post.name}</h1>
-                                        </Link>
-                                        <p className="blog-post-text">
-                                            {post.description.substring(0, 50) + "..."}
-                                        </p>
-                                    </div>
+                    {posts.length > 0 ? (
+                        posts.map((post) => (
+                            <div className="blog-post" key={post.id}>
+                                <div className="blog-post-img">
+                                    <Image src={`/uploads/${post.imageUrl}`} alt={post.imageUrl}/>
                                 </div>
-                   ))}
+                                <div className="blog-post-info">
+                                    <div className="blog-post-date">
+                                        <span>{post.createdAt.toString().substring(0, 10)}</span>
+                                    </div>
+                                    <Link href={`/blog/post/${post.id}`}>
+                                        <h1 className="blog-post-title">{post.name}</h1>
+                                    </Link>
+                                    <p className="blog-post-text">
+                                        {post.description.substring(0, 50) + "..."}
+                                    </p>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No posts available</p>
+                    )}
+
                 </div>
             </main>
         );
