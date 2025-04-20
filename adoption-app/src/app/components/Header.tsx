95dd5f2f47ae780e057167b758d171b7f5c56a79
Олександр Пархоменко <42373411+senior-cherry@users.noreply.github.com>
2025-04-20 12:05:08 +0300

    refactor: change layout for header

--- adoption-app/src/app/components/Header.tsx
+++ adoption-app/src/app/components/Header.tsx
@@ -1,6 +1,18 @@
 "use client";
 
-import { Image, Flex, Button, HStack, Box, Menu, MenuButton, IconButton, MenuList, MenuItem } from '@chakra-ui/react';
+import {
+    Image,
+    Flex,
+    Button,
+    HStack,
+    Box,
+    Menu,
+    MenuButton,
+    IconButton,
+    MenuList,
+    MenuItem,
+    Divider,
+} from '@chakra-ui/react';
 import Link from "next/link";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import {
@@ -13,10 +25,9 @@ import {
 } from "@chakra-ui/icons";
 import LanguageSwitcher from "@/app/components/LanguageSwitcher";
 
-
 const Header = () => {
     return (
-        <Box className="header">
+        <Box className="header" w="100%">
             <Flex
                 w="100%"
                 px="6"
@@ -25,7 +36,7 @@ const Header = () => {
                 justify="space-between"
             >
                 <Link href="/">
-                    <Box w="280px" h="75px">
+                    <Box w="260px" h="60px">
                         <Image
                             src="/uploads/logo_w_bg-removebg-preview.png"
                             alt="Paw in Paw"
@@ -35,7 +46,8 @@ const Header = () => {
                         />
                     </Box>
                 </Link>
-                <div className="mobile-menu">
+
+                <Box className="mobile-menu" display={{ base: "block", xl: "none" }}>
                     <Menu>
                         <MenuButton
                             as={IconButton}
@@ -43,55 +55,51 @@ const Header = () => {
                             icon={<HamburgerIcon />}
                         />
                         <MenuList>
-                            <Link href="/about">
-                                <MenuItem icon={<InfoIcon />} color={"black"}>
-                                    Про нас
-                                </MenuItem>
-                            </Link>
-                            <Link href="/pets">
-                                <MenuItem icon={<SearchIcon />} color={"black"}>
-                                    Улюбленці
-                                </MenuItem>
-                            </Link>
-                            <Link href="/blog">
-                                <MenuItem icon={<CalendarIcon />} color={"black"}>
-                                    Блог
-                                </MenuItem>
-                            </Link>
-                            <Link href="/chat">
-                                <MenuItem icon={<ChatIcon />} color={"black"}>
-                                    Чат
-                                </MenuItem>
-                            </Link>
-                            <Link href="/dashboard">
-                                <MenuItem icon={<SettingsIcon />} color={"black"}>
-                                    Адміністративна панель
-                                </MenuItem>
-                            </Link>
+                            <Box px={4} py={2} color="black">
+                                <SignedOut>
+                                    <SignInButton />
+                                </SignedOut>
+                                <SignedIn>
+                                    <UserButton afterSignOutUrl="/" />
+                                </SignedIn>
+                            </Box>
+
+                            <Divider my={2} />
+
+                            <Link href="/about"><MenuItem icon={<InfoIcon />} color="black">Про нас</MenuItem></Link>
+                            <Link href="/pets"><MenuItem icon={<SearchIcon />} color="black">Улюбленці</MenuItem></Link>
+                            <Link href="/blog"><MenuItem icon={<CalendarIcon />} color="black">Блог</MenuItem></Link>
+                            <Link href="/chat"><MenuItem icon={<ChatIcon />} color="black">Чат</MenuItem></Link>
+                            <Link href="/dashboard"><MenuItem icon={<SettingsIcon />} color="black">Адмінпанель</MenuItem></Link>
+
+                            <Divider my={2} />
+
+                            <Box px={4} py={2}>
+                                <LanguageSwitcher />
+                            </Box>
                         </MenuList>
                     </Menu>
-                </div>
-                <HStack as="nav" spacing="5" fontSize={32} letterSpacing={2} className="menu">
-                    <Link href="/about">
-                        <Button variant="nav">Про нас</Button>
-                    </Link>
-                    <Link href="/pets">
-                        <Button variant="nav">Улюбленці</Button>
-                    </Link>
-                    <Link href="/blog">
-                        <Button variant="nav">Блог</Button>
-                    </Link>
-                    <Link href="/chat">
-                        <Button variant="nav">Чат</Button>
-                    </Link>
-                    <Link href="/dashboard">
-                        <Button variant="nav">Адміністративна панель</Button>
-                    </Link>
+                </Box>
+
+                <HStack
+                    as="nav"
+                    spacing="5"
+                    fontSize={32}
+                    letterSpacing={2}
+                    className="menu"
+                    display={{ base: "none", xl: "flex" }}
+                >
+                    <Link href="/about"><Button variant="nav">Про нас</Button></Link>
+                    <Link href="/pets"><Button variant="nav">Улюбленці</Button></Link>
+                    <Link href="/blog"><Button variant="nav">Блог</Button></Link>
+                    <Link href="/chat"><Button variant="nav">Чат</Button></Link>
+                    <Link href="/dashboard"><Button variant="nav">Адмінпанель</Button></Link>
                 </HStack>
-                <HStack>
+
+                <HStack display={{ base: "none", xl: "flex" }}>
                     <LanguageSwitcher />
                 </HStack>
-                <HStack>
+                <HStack display={{ base: "none", xl: "flex" }}>
                     <SignedOut>
                         <SignInButton />
                     </SignedOut>
