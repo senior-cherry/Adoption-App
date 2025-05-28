b8cfa377cf87776aa30df38ffa68583d24b60007
Олександр Пархоменко <42373411+senior-cherry@users.noreply.github.com>
2025-05-28 23:43:15 +0300

    feat: add CategoryType to DrawerFilter component

--- adoption-app/src/components/DrawerFilter.tsx
+++ adoption-app/src/components/DrawerFilter.tsx
@@ -15,10 +15,11 @@ import {
 import CategoryDropdown from "@/components/CategoryDropdown";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useTranslations } from 'next-intl';
+import {CategoryType} from "@/types/types";
 
 const DrawerFilter = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState<CategoryType[]>([]);
     const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
     const searchParams = useSearchParams();
