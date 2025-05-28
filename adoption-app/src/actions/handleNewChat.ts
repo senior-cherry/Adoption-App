f4602868c0a0fb5608693aa61b48a227fecd882c
Олександр Пархоменко <42373411+senior-cherry@users.noreply.github.com>
2025-05-28 15:52:55 +0300

    feat: add null checkup for new chat

--- adoption-app/src/actions/handleNewChat.ts
+++ adoption-app/src/actions/handleNewChat.ts
@@ -1,27 +1,29 @@
 'use server';
-
 import { revalidatePath } from 'next/cache';
-import {prisma} from "@/utils/connect";
-import {auth} from "@clerk/nextjs/server";
+import { prisma } from "@/utils/connect";
+import { auth } from "@clerk/nextjs/server";
 
 export const handleNewChat = async (formData: FormData): Promise<void> => {
     const chatName = formData.get('chatName') as string | null;
-    const userId = auth();
+    const { userId } = auth();
 
-    if (chatName) {
-        try {
-            await prisma.chat.create({
-                data: {
-                    name: chatName,
-                    user_id: userId.userId
-                },
-            });
+    if (!chatName || !userId) {
+        console.error("Missing chat name or user ID");
+        return;
+    }
 
-            revalidatePath('/');
-        } catch (error) {
-            console.error("Error creating new chat", error);
-        } finally {
-            await prisma.$disconnect();
-        }
+    try {
+        await prisma.chat.create({
+            data: {
+                name: chatName,
+                user_id: userId,
+            },
+        });
+
+        revalidatePath('/');
+    } catch (error) {
+        console.error("Error creating new chat", error);
+    } finally {
+        await prisma.$disconnect();
     }
 };
